3013543f91fa606c818d224fa93398ff9f8549a2
Nelson Plíni <nelsonplinio94@gmail.com>
2020-07-23 22:12:18 -0300

    Added rotes validations

--- src/modules/tools/infra/http/routes/index.ts
+++ src/modules/tools/infra/http/routes/index.ts
@@ -1,12 +1,42 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import ToolsController from '../controllers/ToolsController';
 
 const toolsRoutes = Router();
 const toolsController = new ToolsController();
 
-toolsRoutes.post('/', toolsController.create);
-toolsRoutes.get('/', toolsController.index);
-toolsRoutes.delete('/:tool_id', toolsController.delete);
+toolsRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      link: Joi.string().required(),
+      tags: Joi.array().required(),
+    },
+  }),
+  toolsController.create,
+);
+
+toolsRoutes.get(
+  '/',
+  celebrate({
+    [Segments.QUERY]: {
+      tag: Joi.string(),
+    },
+  }),
+  toolsController.index,
+);
+
+toolsRoutes.delete(
+  '/:tool_id',
+  celebrate({
+    [Segments.PARAMS]: {
+      tool_id: Joi.string().required(),
+    },
+  }),
+  toolsController.delete,
+);
 
 export default toolsRoutes;
